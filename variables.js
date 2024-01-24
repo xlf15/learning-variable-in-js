39c9524516500523f613f60e385cfc78bceb05ea
sathya [xlf15] <107190769+xlf15@users.noreply.github.com>
2024-01-24 20:27:03 +0530

    variable in js
    simple variable learning code of js

---
+++ variables.js
@@ -0,0 +1,32 @@
+// console.log("hello world");
+// console.log("this is my first code of javascript");
+// console.log("my name is sathya sachi paria!");
+
+
+
+// console.log("this is the for the imfomation section part");
+
+// const product_detail = {
+//     name : " parker pen ",
+//     ration : 1 ,
+//     price : 250 ,
+//     offer : 5
+// };
+
+
+// console.log(product_detail);
+
+console.log("~ this is for instagram profile ~");
+
+const profile ={
+   username : "sathyaxlf15@",
+   isfllow : true ,
+   post : 27 ,
+   following : 470 ,
+   following : 1500 ,
+   name : "sathya sachi paira" ,
+   threadlink : "@sathyaxlf15" ,
+   discription:" security analyst , programmer , bug hunter , maldev "
+
+};
+console.log(profile)
